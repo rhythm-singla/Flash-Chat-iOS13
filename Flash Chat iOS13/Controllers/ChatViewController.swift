ab65d0c29ea056d4aa036f04ba2ad6f2100d5ff9
Rhythm Singla <rhythm.singla@sugarboxnetworks.com>
2022-02-12 12:29:47 +0530

    Commit: Registration, Login, and LogOut Completed.

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
 
@@ -15,9 +16,20 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "⚡️FlashChat"
+        navigationItem.hidesBackButton = true
     }
     
+    @IBAction func signOutClicked(_ sender: UIButton) {
+            let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+      
+    }
     @IBAction func sendPressed(_ sender: UIButton) {
     }
     
